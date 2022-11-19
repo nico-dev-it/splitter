ef80b819dffbe8cb6de93fa1c6d454509b7c8ac2
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-11-19 17:26:02 +0000

    splitter project

---
+++ script.js
@@ -0,0 +1,72 @@
+const form = document.getElementById("form")
+const totalText = document.getElementById("total")
+const tipText = document.getElementById("tipPerPerson")
+const error = document.getElementById("error")
+const customTipField2 = document.getElementById("tip")
+const resetButton = document.getElementById("reset")
+const toggleButton = document.getElementById("toggle")
+
+toggleButton.addEventListener('click', () => {
+    let body = document.querySelector('.body')
+    body.classList.toggle('light')
+})
+
+function calculator() {
+    const billAmount = document.getElementById("billAmount").value
+    const regex = new RegExp("^0+(?!$)", 'g');
+    const numberOfPeople = document.getElementById("numberOfPeople").value
+    let tip = document.querySelector("input[type = 'radio']:checked").value
+    let total = (((tip * billAmount) / numberOfPeople).toFixed(2)) * 1
+    let tipAmount = (((tip * billAmount) - billAmount) / numberOfPeople).toFixed(2)
+
+    if (numberOfPeople === "0") {
+        error.textContent = "Can't be 0!"
+    }
+
+    if (isNaN(total) || (total === Infinity)) {
+        totalText.textContent = "0.00"
+
+    } else {
+        totalText.textContent = total
+        tipText.textContent = tipAmount
+    }
+}
+
+function customTip() {
+    const billAmount = document.getElementById("billAmount").value
+    const numberOfPeople = document.getElementById("numberOfPeople").value
+    let customTip = document.getElementById("tip").value
+    let total = (((customTip * billAmount) / numberOfPeople).toFixed(2)) * 1
+    let tipAmount = (((customTip * billAmount) - billAmount) / numberOfPeople).toFixed(2)
+
+    if (numberOfPeople === "0") {
+        error.textContent = "Can't be 0!"
+    }
+
+    if (isNaN(total) || (total === Infinity)) {
+        totalText.textContent = "0.00"
+
+    } else {
+        totalText.textContent = total
+        tipText.textContent = tipAmount
+    }
+}
+
+customTipField2.addEventListener('input', (event) => {
+});
+oninput = (event) => {
+    customTip()
+};
+
+form.addEventListener('change', function (event) {
+    event.preventDefault()
+    calculator()
+})
+
+resetButton.addEventListener("click", function (event) {
+    totalText.textContent = "0.00"
+    tipText.textContent = "0.00"
+    form.reset()
+
+})
+
